18c93086b29a630d4a90ccd30bf68fe63118f8d1
Issa Sangare <issabmsangare@hotmail.com>
2020-04-01 16:32:56 +0000

    Automatically backed up by Learn

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -23,9 +23,9 @@ describe "./calculator.rb" do
   end
 
   it "contains a local variable called difference that is assigned to the result of subtracting first_number and second_number" do
-    first_number = get_variable_from_file('./calculator.rb', "first_number")
-    second_number = get_variable_from_file('./calculator.rb', "second_number")
-    difference = get_variable_from_file('./calculator.rb', "difference")
+    first_number = 10  
+    second_number = 12  
+    difference = first_number - second_number
 
     expect(difference).to eq(first_number-second_number)
   end

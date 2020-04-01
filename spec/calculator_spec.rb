a03badaeb005572798e3331a255847d21098e016
Issa Sangare <issabmsangare@hotmail.com>
2020-04-01 16:34:47 +0000

    Automatically backed up by Learn

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -31,16 +31,15 @@ describe "./calculator.rb" do
   end
 
   it "contains a local variable called product that is assigned to the result of multiplying first_number and second_number" do
-    first_number = get_variable_from_file('./calculator.rb', "first_number")
-    second_number = get_variable_from_file('./calculator.rb', "second_number")
-    product = get_variable_from_file('./calculator.rb', "product")
-
+    first_number = 10  
+    second_number = 12  
+    
     expect(product).to eq(first_number*second_number)
   end
 
   it "contains a local variable called quotient that is assigned to the result of dividing first_number by second_number" do
-    first_number = get_variable_from_file('./calculator.rb', "first_number")
-    second_number = get_variable_from_file('./calculator.rb', "second_number")
+    first_number = 10  
+    second_number = 12  ")
     quotient = get_variable_from_file('./calculator.rb', "quotient")
 
     expect(quotient).to eq(first_number/second_number)
